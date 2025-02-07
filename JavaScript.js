df3c3525f465f1cc266a52b8cdcd613892ce2538
cas1092 <castillo.goche.l@gmail.com>
2025-02-07 18:58:06 -0500

    final

---
+++ JavaScript.js
@@ -0,0 +1,15 @@
+﻿const btn = document.getElementsByClassName('question');
+
+for (var i = 0; i < btn.length; i++) {
+    btn[i].addEventListener('click', function(){
+        const x = this.nextElementSibling;
+        this.classList.toggle('active');
+        if (x.style.display === 'block') {
+            x.style.display = 'none';
+        }
+        else {
+            x.style.display = 'block';
+            
+        }
+    })
+}
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Accordion-component
  
+ https://roadmap.sh/projects/accordion
